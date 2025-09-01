59147b17cb0ccc57c96451e6046069bc65c4ff56
govardhan <govardhanxo@gmail.com>
2025-09-01 14:45:36 +0530

    terraform-aws-eks

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "latest-govardhanxo-remote-state-dev"
     key    = "roboshop-dev-sg"
     region = "us-east-1"
     encrypt        = true
